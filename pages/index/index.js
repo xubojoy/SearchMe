b450259c550dd8dce500b57f45193ff02d2ba371
xubojoy <xubojoy1112@163.com>
2017-06-26 16:29:47 +0800

    提交简单版

---
+++ pages/index/index.js
@@ -0,0 +1,35 @@
+//index.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    // motto: 'Hello World',
+    // userInfo: {}
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    console.log('onLoad')
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function(userInfo){
+      //更新数据
+      that.setData({
+        userInfo:userInfo
+      })
+    })
+  },
+  onShareAppMessage: function () {
+    // return custom share data when user share.
+    console.log('onShareAppMessage')
+    return {
+      title: '查我',
+      desc: '小程序',
+      path: '/pages/index/index'
+    }
+  },
+})
