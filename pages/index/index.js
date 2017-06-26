abb162214c58d0fa1be19c1994458d9f63cfcd37
xubojoy <xubojoy1112@163.com>
2017-06-26 17:55:52 +0800

    优化为动态

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,13 +3,11 @@
 var app = getApp()
 Page({
   data: {
-    // motto: 'Hello World',
-    // userInfo: {}
   },
   //事件处理函数
   bindViewTap: function() {
     wx.navigateTo({
-      url: '../logs/logs'
+
     })
   },
   onLoad: function () {
@@ -21,6 +19,36 @@ Page({
       that.setData({
         userInfo:userInfo
       })
+    })
+  },
+  chooseImageTap: function () {
+    let _this = this;
+    wx.showActionSheet({
+      itemList: ['从相册中选择', '拍照'],
+      itemColor: "#f7982a",
+      success: function (res) {
+        if (!res.cancel) {
+          if (res.tapIndex == 0) {
+            _this.chooseWxImage('album')
+          } else if (res.tapIndex == 1) {
+            _this.chooseWxImage('camera')
+          }
+        }
+      }
+    })
+
+  },
+  chooseWxImage: function (type) {
+    let _this = this;
+    wx.chooseImage({
+      sizeType: ['original', 'compressed'],
+      sourceType: [type],
+      success: function (res) {
+        console.log(res);
+        _this.setData({
+          logo: res.tempFilePaths[0],
+        })
+      }
     })
   },
   onShareAppMessage: function () {
