e95ead065b224307a11821ee4c3c93bc8798ae82
xubojoy <xubojoy1112@163.com>
2017-07-05 17:59:18 +0800

    添加搜索

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,5 @@
 var fileData = require('../../utils/config.js')
+var WxSearch = require('../../wxSearch/wxSearch.js')
 var app = getApp()
 Page({
   data: {
@@ -12,6 +13,9 @@ Page({
 
   onLoad: function () {
     var that = this;
+    //初始化的时候渲染wxSearchdata
+    WxSearch.init(that, 43, ['weappdev', '小程序', 'wxParse', 'wxSearch', 'wxNotification']);
+    WxSearch.initMindKeys(['weappdev.com', '微信小程序开发', '微信开发', '微信小程序']);
     var data = that.data.showData;
     that.setData({ 
       companys: data
@@ -22,6 +26,39 @@ Page({
    wx.navigateTo({
      url: '../addCompany/addCompany',
    })
+  },
+  wxSearchFn: function (e) {
+    var that = this
+    WxSearch.wxSearchAddHisKey(that);
+
+  },
+  wxSearchInput: function (e) {
+    var that = this
+    WxSearch.wxSearchInput(e, that);
+  },
+  wxSerchFocus: function (e) {
+    var that = this
+    WxSearch.wxSearchFocus(e, that);
+  },
+  wxSearchBlur: function (e) {
+    var that = this
+    WxSearch.wxSearchBlur(e, that);
+  },
+  wxSearchKeyTap: function (e) {
+    var that = this
+    WxSearch.wxSearchKeyTap(e, that);
+  },
+  wxSearchDeleteKey: function (e) {
+    var that = this
+    WxSearch.wxSearchDeleteKey(e, that);
+  },
+  wxSearchDeleteAll: function (e) {
+    var that = this;
+    WxSearch.wxSearchDeleteAll(that);
+  },
+  wxSearchTap: function (e) {
+    var that = this
+    WxSearch.wxSearchHiddenPancel(that);
   },
   onShareAppMessage: function () {
     // return custom share data when user share.
