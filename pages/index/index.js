637e80321a2625870f5d05c8412999cbec4f1b24
xubojoy <xubojoy1112@163.com>
2017-07-06 18:11:50 +0800

    优化

--- pages/index/index.js
+++ pages/index/index.js
@@ -12,14 +12,15 @@ Page({
   },
 
   onLoad: function () {
+    console.log('----onLoad-------');
     var that = this;
-    //初始化的时候渲染wxSearchdata
-    WxSearch.init(that, 43, ['weappdev', '小程序', 'wxParse', 'wxSearch', 'wxNotification']);
-    WxSearch.initMindKeys(['weappdev.com', '微信小程序开发', '微信开发', '微信小程序']);
     var data = that.data.showData;
     that.setData({ 
       companys: data
     });
+    //初始化的时候渲染wxSearchdata
+    WxSearch.init(that, 43, ['北京百度网讯科技有限公司', '阿里巴巴（中国）网络技术有限公司', '深圳市腾讯计算机系统有限公司']);
+    WxSearch.initMindKeys(['https://www.baidu.com/', 'http://www.alibaba.com/', 'http://www.tencent.com/']);
   },
   addBtnClick: function (){
     console.log('-------点击了添加公司')
@@ -28,15 +29,16 @@ Page({
    })
   },
   wxSearchFn: function (e) {
+    console.log('----wxSearchFn-------');
     var that = this
     WxSearch.wxSearchAddHisKey(that);
-
   },
   wxSearchInput: function (e) {
     var that = this
     WxSearch.wxSearchInput(e, that);
   },
   wxSerchFocus: function (e) {
+    console.log('----wxSerchFocus-------');
     var that = this
     WxSearch.wxSearchFocus(e, that);
   },
@@ -45,8 +47,10 @@ Page({
     WxSearch.wxSearchBlur(e, that);
   },
   wxSearchKeyTap: function (e) {
+    // console.log('----wxSearchKeyTap-------');
     var that = this
     WxSearch.wxSearchKeyTap(e, that);
+    console.log('----wxSearchKeyTap-------'+e);
   },
   wxSearchDeleteKey: function (e) {
     var that = this
@@ -57,8 +61,12 @@ Page({
     WxSearch.wxSearchDeleteAll(that);
   },
   wxSearchTap: function (e) {
+    console.log('----wxSearchTap-------');
     var that = this
     WxSearch.wxSearchHiddenPancel(that);
+  },
+  searchResultClick: function(){
+    console.log('----searchResultClick-------');
   },
   onShareAppMessage: function () {
     // return custom share data when user share.
